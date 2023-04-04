f5eb5da1799c8e192b4f20ff29a6c0e720b35dd8
Arim4 <127195894+Arim4@users.noreply.github.com>
2023-04-04 12:09:45 +0200

    test add wall

--- serveur_partie.py
+++ serveur_partie.py
@@ -11,6 +11,11 @@ from time import sleep, localtime
 from PodSixNet.Server import Server
 from PodSixNet.Channel import Channel
 
+Y_AXIS_LENGTH = 7
+X_AXIS_LENGTH = 7
+BOARD_X_LENGTH = 2 * X_AXIS_LENGTH - 1
+BOARD_Y_LENGTH = 2 * Y_AXIS_LENGTH - 1
+
 class ClientChannel(Channel):
     """
     This is the server representation of a connected client.
@@ -61,21 +66,128 @@ class MyServer(Server):
 
 class Model :
     def __init__(self):
-        pass
+        self.board = self.new_board()
+        self.pawns = {"UP":Pawn((0, BOARD_X_LENGTH // 2)), "DOWN":Pawn((BOARD_Y_LENGTH, BOARD_X_LENGTH // 2))}
     
     def new_board(self):
-        pass
+        board = [[0 for j in range(BOARD_Y_LENGTH)] for i in range(BOARD_X_LENGTH)]
+        for x in range(BOARD_X_LENGTH):
+            for y in range (BOARD_Y_LENGTH):
+                if x % 2 == 0 and y % 2 == 0 :
+                    board[x][y] = Square()
+                else :
+                    board[x][y] = Hole()
+        return board
     
     def move_pawn(self,pawn,location):
         pass
     
     def add_wall(self,location,orientation):
-        pass
+        """
+        on se contente ici de mettre le mur sans vérifier, ce sera fait au préalable
+        """
+        x, y = location
+        if orientation == "UP":
+            self.board[x][y-1].occupied = True
+            self.board[x][y].occupied = True
+            self.board[x][y+1].occupied = True
+        if orientation == "ACROSS":
+            self.board[x+1][y].occupied = True
+            self.board[x][y].occupied = True
+            self.board[x-1][y].occupied = True
+    
+    def remove_wall(self,location,orientation):
+        x, y = location
+        if orientation == "UP":
+            self.board[x][y-1].occupied = False
+            self.board[x][y].occupied = False
+            self.board[x][y+1].occupied = False
+        if orientation == "ACROSS":
+            self.board[x+1][y].occupied = False
+            self.board[x][y].occupied = False
+            self.board[x-1][y].occupied = False    
     
     def test_add_wall(self,location,orientation):
-        test = True
+        x, y = location
+        if x == 0 or y == 0 or x == BOARD_X_LENGTH or y == BOARD_Y_LENGTH:
+            return False
+        if orientation == "UP":
+            if self.board[x][y-1].occupied or self.board[x][y].occupied or self.board[x][y+1].occupied :
+                return False
+        if orientation == "ACROSS":
+            if self.board[x-1][y].occupied or self.board[x][y].occupied or self.board[x+1][y].occupied :
+                return False
+        return self.pathfind_test_with_wall_from(location,orientation)
+    
+    def accessible_from(self,location,avoid = None):
+        x,y = location
+        accessible_squares = []
+        #aller à gauche
+        if x != 0 and not self.board[x-1][y].occupied:
+            if not self.board[x-2][y].occupied:
+                accessible_squares.append((x-2,y))
+            elif (x-2,y) != avoid:
+                for square in self.accessible_from((x-2,y),avoid = location):
+                    accessible_squares.append(square)
+        #aller à droite
+        if x != BOARD_X_LENGTH and not self.board[x+1][y].occupied:
+            if not self.board[x+2][y].occupied:
+                accessible_squares.append((x+2,y))
+            elif (x+2,y) != avoid:
+                for square in self.accessible_from((x+2,y),avoid = location):
+                    accessible_squares.append(square)
+        #aller en haut
+        if y != 0 and not self.board[x][y-1].occupied:
+            if not self.board[x][y-2].occupied:
+                accessible_squares.append((x,y-2))
+            elif (x,y-2) != avoid:
+                for square in self.accessible_from((x,y-2),avoid = location):
+                    accessible_squares.append(square)
+        #aller en bas
+        if y != BOARD_Y_LENGTH and not self.board[x][y+1].occupied:
+            if not self.board[x][y+2].occupied:
+                accessible_squares.append((x,y+2))
+            elif (x,y+2) != avoid:
+                for square in self.accessible_from((x,y+2),avoid = location):
+                    accessible_squares.append(square)
+        return accessible_squares
+    
+    def pathfind_test_with_wall(self,location,orientation):
+        self.add_wall(location, orientation)
+        for key,pawn in self.pawns:
+            if key == "UP":
+                test = self.pathfind_test(pawn.coords, 0)
+            else :
+                test = self.pathfind_test(pawn.coords, BOARD_Y_LENGTH)
+        self.remove_wall(location, orientation)
         return test
     
-    def test_move_pawn(self,pawn,location):
-        test = True
-        return test
+    def pathfind_test(self,location,targeted_y,all_accessibles=[],new_accessibles=[]):
+        if all_accessibles == []:
+            acc = list(self.accessible_from(location))
+            new_accessibles = acc
+            all_accessibles = acc
+        else: 
+            border = []
+            for new_square in new_accessibles:
+                for square in self.accessible_from(new_square):
+                    if square not in all_accessibles:
+                        if square[1] == targeted_y:
+                            return True
+                        border.append(square)
+                        all_accessibles.append(square)
+        if border == []:
+            return False
+        return self.pathfind_test(location,targeted_y,all_accessibles=all_accessibles,new_accessibles=border)
+
+class Pawn :
+    def __init__(self,coords):
+        self.coords = coords
+        
+class Hole :
+    def __init__(self):
+        self.occupied = False
+        
+class Square :
+    def __init__(self):
+        self.occupied = False
