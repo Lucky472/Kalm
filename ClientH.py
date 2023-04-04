ae856095aa004b48d0daec28677c46de602514ce
KikiTheLord <82971844+KikiTheLord@users.noreply.github.com>
2023-04-04 19:28:48 +0200

    début affichage jeu + création fichier pour tester le code tant que le réseau marche pas

--- ClientH.py
+++ ClientH.py
@@ -12,6 +12,8 @@ Y_AXIS_LENGTH = 7
 X_AXIS_LENGTH = 7
 BOARD_X_LENGTH = 2 * X_AXIS_LENGTH - 1
 BOARD_Y_LENGTH = 2 * Y_AXIS_LENGTH - 1
+SIZESQUARE = 10
+COLORSQUARE = "#A567A3"
 
 HOST, PORT = "localhost", "31425"
 NICKNAME = "nick"
@@ -76,6 +78,21 @@ class ClientWindow(Tk):
             self.client.Loop()
             sleep(0.001)
         exit()    
+
+class View:
+    def __init__(self,window,board):
+        self.window = window
+        self.canvas_board = Canvas(self.window)
+        self.draw_board(board)
+    
+    def draw_board(self,board):
+        for x in range(BOARD_X_LENGTH):
+            for y in range (BOARD_Y_LENGTH):
+                if self.is_square(x,y):
+                    self.canvas_board.create_rectangle(x*SIZESQUARE,y*SIZESQUARE,(x+1)*SIZESQUARE,(y+1)*SIZESQUARE)
+
+    def is_square(self,x,y):
+        return (x % 2 == 0) and (y % 2 == 0)
 class Model :
     def __init__(self):
         self.board = self.new_board()
@@ -85,11 +102,14 @@ class Model :
         board = [[0 for j in range(BOARD_Y_LENGTH)] for i in range(BOARD_X_LENGTH)]
         for x in range(BOARD_X_LENGTH):
             for y in range (BOARD_Y_LENGTH):
-                if x % 2 == 0 and y % 2 == 0 :
+                if self.is_square(x,y):
                     board[x][y] = Square()
                 else :
                     board[x][y] = Hole()
         return board
+    
+    def is_square(self,x,y):
+        return (x % 2 == 0) and (y % 2 == 0)
     
     def move_pawn(self,pawn,location):
         pass
---
+++ TestPourJeu.py
@@ -0,0 +1,47 @@
+from tkinter import *
+Y_AXIS_LENGTH = 7
+X_AXIS_LENGTH = 7
+BOARD_X_LENGTH = 2 * X_AXIS_LENGTH - 1
+BOARD_Y_LENGTH = 2 * Y_AXIS_LENGTH - 1
+SIZESQUARE = 10
+COLORSQUARE = "#A567A3"
+
+class View:
+    def __init__(self,window,board):
+        self.window = window
+        self.canvas_board = Canvas(self.window)
+        self.draw_board(board)
+        self.canvas_board.pack()
+    
+    def draw_board(self,board):
+        for x in range(BOARD_X_LENGTH):
+            for y in range (BOARD_Y_LENGTH):
+                if self.is_square(x,y):
+                    self.canvas_board.create_rectangle(x*SIZESQUARE,y*SIZESQUARE,(x+1)*SIZESQUARE,(y+1)*SIZESQUARE,fill = COLORSQUARE)
+
+    def is_square(self,x,y):
+        return (x % 2 == 0) and (y % 2 == 0)
+
+class Square:
+    def __init__(self):
+        pass
+class Hole:
+    def __init__(self):
+        pass
+
+board = [[0 for j in range(BOARD_Y_LENGTH)] for i in range(BOARD_X_LENGTH)]
+def return_board(board):
+    for x in range(BOARD_X_LENGTH):
+        for y in range (BOARD_Y_LENGTH):
+            if (x%2 == 0) and (y%2 ==0):
+                board[x][y] = Square()
+            else :
+                board[x][y] = Hole()
+    return board
+
+
+
+window = Tk()
+view = View(window,return_board(board))
+window.mainloop()
+
