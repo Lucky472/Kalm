9a2a399b278f7d825b23eea79257c7d233850271
toulpolovik <127196415+toulpolovik@users.noreply.github.com>
2023-04-06 23:20:30 +0200

    Update client_menu.py

--- client_menu.py
+++ client_menu.py
@@ -125,7 +125,7 @@ class Menu :
     def afficher_liste_joueur(self,dico_list):
         for i in range(len(self.Liste_joueur)-2,-1,-2):
             self.L_joueur=Label(self.buttons.f_liste,text=self.Liste_joueur[i]+" "+ str(self.Liste_joueur[i+1]),font=("arial",10),bg='#4065A4',fg='black',bd=2,relief=SUNKEN)
-            self.L_joueur.pack(pady=i) 
+            self.L_joueur.pack(pady=1) 
             
     def trier_liste_score(self,dico_list):
         self.Liste_score_joueur=[]
